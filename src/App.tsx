27935ccf76612f9f2789db15ef8a78854995d9bf
Aa <aa@example.com>
2025-03-14 15:41:58 +0200

    added basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,15 @@ function App() {
 
   return (
     <>
-      <h1>Hello Tailwind</h1>
+      <h1 className="text-5xl font-bold text-orange-400 text-center mt-20">Hello Tailwind</h1>
+
+        <button className="bg-amber-500 hover:bg-orange-400 rounded-2xl mt-10 px-4 py-2 mx-auto block border border-white/50 border-solid">
+            Click Me!
+        </button>
+
+        <div className="bg-blue-400 mt-6 w-24 h-32">
+            Width and Height
+        </div>
     </>
   )
 }
